c68428ff19a4c762835148f814fc7c6be2aed67e
Dhara Nakum <17comp.dharaa.nakum@gmail.com>
2020-02-26 21:49:44 +0530

    celsius to fahrenheit

---
+++ practical9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practical9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value...");
+            string number = Console.ReadLine();
+            float num = float.Parse(number);
+            Console.WriteLine("if you want to convert celsius to fahrenheit then press 1");
+            Console.WriteLine("else press 2");
+            string ch = Console.ReadLine();
+            int c1 = Int32.Parse(ch);
+            if (c1==1)
+            {
+                float f;
+                f = (float)(((9 / 5) * num) + 32) / 32;
+                Console.WriteLine("fahrenheit : " + f);
+            }
+            else
+            {
+                float c;
+                double a = 0.55555556;
+                c = (float)(((32 * num)-32)*a);
+                //c = (float)((5 / 9) * ((32 * num) - 32));
+                Console.WriteLine("celsius : " + c);
+            }
+            Console.Read();
+        }
+    }
+}
